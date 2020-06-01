4416ae62c1ead7ef7f67ed45f879b3db57ba07ba
Seth H <seth.holobaugh@gmail.com>
2020-05-31 22:31:08 -0500

    Updated test.js; created gitignore file

--- dev/test.js
+++ dev/test.js
@@ -1,17 +1,16 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
+const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
 
-bitcoin.createNewTransaction(100,'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'B4CEE9C0E5CD571',
+        recipient: '3A3F6E462D48E9',
+    }
+];
 
-bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
+const nonce = 100;
 
-bitcoin.createNewTransaction(50,'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(200,'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-bitcoin.createNewTransaction(300,'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
-
-bitcoin.createNewBlock(397454, '8CE765D75E6S1RS9', 'A6B7T14R7T6S4J8G');
-
-console.log(bitcoin);
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
