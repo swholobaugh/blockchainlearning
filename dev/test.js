a5cdea47557e5d8bd41a16de4a672ca0db4f9a04
Seth H <seth.holobaugh@gmail.com>
2020-05-31 22:26:10 -0500

    Add sha256, created hashBlock function

--- dev/test.js
+++ dev/test.js
@@ -8,4 +8,10 @@ bitcoin.createNewTransaction(100,'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
 
 bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WPLS214R7T6SJ3G2');
 
-console.log(bitcoin.chain[0]);
+bitcoin.createNewTransaction(50,'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+bitcoin.createNewTransaction(200,'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+bitcoin.createNewTransaction(300,'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+
+bitcoin.createNewBlock(397454, '8CE765D75E6S1RS9', 'A6B7T14R7T6S4J8G');
+
+console.log(bitcoin);
