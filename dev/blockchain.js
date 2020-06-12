5aee7a3eb06f1507f23c2ef7215e27cbbe0cd5f9
swholobaugh <seth.holobaugh@gmail.com>
2020-06-12 14:38:03 -0500

    Completed Blockchain

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -4,6 +4,7 @@ const sha256 = require('sha256');
 function Blockchain () {
     this.chain = [];
     this.pendingTransactions = [];
+    this.createNewBlock(100, '0', '0');
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
@@ -42,6 +43,17 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
     const hash = sha256(dataAsString);
 
     return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+    while(hash.substring(0,4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+    return nonce;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,16 +1,28 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
-const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
+console.log(bitcoin);
+
+/*
+const previousBlockHash = 'OINAISDFN09N09ASDNF90N90ASNDF';
 
 const currentBlockData = [
     {
-        amount: 10,
-        sender: 'B4CEE9C0E5CD571',
-        recipient: '3A3F6E462D48E9',
-    }
+        amount: 101,
+        sender: 'N90ANS90N90ANSDFN',
+        recipient: '90NA90SNDF90ANSDF09N',
+    },
+    {
+        amount: 30,
+        sender: '09ANS09DFNA8SDNF',
+        recipient: 'UIANSIUDFUIABSDUIFB',
+    },
+    {
+        amount: 200,
+        sender: '89ANS89DFN98ASNDF89',
+        recipient: 'AUSDF89ANSD9FNASD',
+    },
 ];
 
-const nonce = 100;
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+*/
